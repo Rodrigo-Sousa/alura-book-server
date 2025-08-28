68e88e5450149632f53624c54819ea437925e98b
Rodrigo Sousa - Work <rodrigo.sdeveloper@gmail.com>
2025-08-28 16:44:33 -0300

    Adicionado o conteúdo para leitura do arquivo, com a função 'fs - filesystem', na reqisição GET

--- controllers/livros.js
+++ controllers/livros.js
@@ -1,7 +1,12 @@
+// Importando o fs
+const fs = require("fs");
+
 // Controlador responsável pela regra de negócio das requisições para livros
 function getLivros(req, res) {
     try {
-        res.send("Entrado na rota GET, pelo controaldor!");
+        // lendo os arquivos do JSON
+        const livros = JSON.parse(fs.readFileSync("./data/livros.json"));
+        res.send(livros);
     } catch (error) {
         res.status(500);
         res.send(error.message);
